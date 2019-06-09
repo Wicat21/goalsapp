cdb5544bf38d08a5287c4735d82396f33fc493f0
Vera\Wicat <weruss.kiss@gmail.com>
2019-06-09 19:30:05 +0200

    időbeállítás, switch done, push notification nem csak akkor kell, amikor háttérben van az app - még nem megy háttérből sem

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -6,22 +6,24 @@ import {
   TouchableOpacity,
   Picker,
   AppState,
-  Platform
+  Platform,
+  Switch
 } from "react-native";
 import PushController from './PushController';
 import PushNotification from 'react-native-push-notification';
+import TimePicker from "react-native-24h-timepicker";
 
 class Settings extends Component {
     constructor(props) {
         super(props);
         this.handleAppStateChange = this.handleAppStateChange.bind(this);
         this.state = {
-          hour: '5',
+            switchValue: false,
+            hour: '5',
+            time: '',
+            hour: null, 
+            minute: null
         };
-        /*let date = new Date(Date.now() + (this.state.hour * 1000));
-        if (Platform.OS === 'ios') {
-            date = date.toISOString();
-        }*/
     }
      
     componentDidMount(){
@@ -32,31 +34,68 @@ class Settings extends Component {
         AppState.removeEventListener('change', this.handleAppStateChange);
     }
     handleAppStateChange(appState){
-        if (appState === 'background') {
+        if (date.getHours()==hour && date.getMinutes()==minute && appState === 'background') {
             PushNotification.localNotificationSchedule({
                 //... You can use all the options from localNotifications
-                message: "My Notification Message", // (required)
-                date: new Date(Date.now() + (this.state.hour * 1000)) // in 60 secs
+                message: "Don't forget your goals!", // (required)
+                date: new Date(Date.now() + (1000)) // in 60 secs
               });
         }
     }
+
+    onCancel() {
+        this.TimePicker.close();
+      }
+     
+      onConfirm(hour, minute) {
+        this.setState({ time: `${hour}:${minute}`, hour:hour, minute:minute });
+        this.TimePicker.close();
+      }
+
+    toggleSwitch = (value) => {
+        this.setState({switchValue: value});
+    }
+
+    renderNotification(){
+        if (this.state.switchValue === true){
+            return(
+                <View>
+                    <TouchableOpacity
+                        onPress={() => this.TimePicker.open()}
+                        style={styles.button}
+                    >
+                        <Text style={styles.buttonText}>Set Notification time</Text>
+                    </TouchableOpacity>
+                    <View>
+                        <Text>Notification time:</Text>
+                        <Text style={styles.text}>{this.state.time}</Text>
+                    </View>
+                    <TimePicker
+                        ref={ref => {
+                            this.TimePicker = ref;
+                        }}
+                        onCancel={() => this.onCancel()}
+                        onConfirm={(hour, minute) => this.onConfirm(hour, minute)}
+                    />
+                </View>
+            );
+        }
+    }
+
     render () {
         return (
             <View>
                 <View style={styles.headerStyle}>
                     <Text style={styles.headerText}>Settings</Text>
                 </View>
-                <Text>Push Notification</Text>
-                <Picker
-                    selectedValue={this.state.hour}
-                    style={{height: 100, width: 120, alignSelf:'center'}}
-                    onValueChange={(itemValue, itemIndex) =>
-                    this.setState({freq: itemValue})
-                    }>
-                    <Picker.Item label="5" value="5" />
-                    <Picker.Item label="09:00" value="09:00" />
-                    <Picker.Item label="10:00" value="10:00" />
-                </Picker>
+                <TouchableOpacity style={[styles.button, {flexDirection:'row'}]}>
+                    <Text style={styles.buttonText}>Enable Notification</Text>
+                    <Switch
+                        onValueChange = {this.toggleSwitch}
+                        value = {this.state.switchValue}
+                    />
+                </TouchableOpacity>
+                {this.renderNotification()}
                 <PushController/>
             </View>
         );
@@ -82,7 +121,22 @@ const styles = {
       fontSize: 20,
       padding: 25,
       color: "white"
-    }
+    },
+    text: {
+        fontSize: 20,
+        marginTop: 10
+      },
+      button: {
+        backgroundColor: "#3c4f76",
+        paddingVertical: 11,
+        paddingHorizontal: 17,
+        borderRadius: 3
+      },
+      buttonText: {
+        color: "#FFFFFF",
+        fontSize: 16,
+        fontWeight: "600"
+      }
 };
 
 export default Settings;
